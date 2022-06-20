dfe3538472021e82df27cd0b90a4bd416aae2622
yuliangzhong <yubaby2003@163.com>
2022-06-20 21:31:46 +0200

    both exact and approximate sync test passed!

--- ekf/src/ekf.cpp
+++ ekf/src/ekf.cpp
@@ -15,6 +15,8 @@
 
 #include "message_filters/subscriber.h"
 #include "message_filters/time_synchronizer.h"
+#include "message_filters/sync_policies/approximate_time.h"
+#include "message_filters/synchronizer.h"
 
 using std::placeholders::_1;
 using std::placeholders::_2;
@@ -27,11 +29,13 @@ using Velocity = interfaces::msg::Velocity;
 using Pose = interfaces::msg::Pose;
 using Acc = interfaces::msg::BodyAcceleration;
 using Omega = interfaces::msg::BodyAngularVelocity;
+using ApproPolicy = message_filters::sync_policies::ApproximateTime<Position, Velocity, Pose, Acc, Omega>;
+using Sync = message_filters::Synchronizer<ApproPolicy>;
 
 class EKF : public rclcpp::Node
 {
   public:
-    EKF() : Node("ekf")
+    EKF() : Node("ekf"), sync_(Sync(ApproPolicy(10), pos_sub_, vel_sub_, pose_sub_, body_acc_sub_, body_omega_sub_))
     {
       pos_sub_.subscribe(this, "dummy_position");
       vel_sub_.subscribe(this, "dummy_velocity");
@@ -39,10 +43,11 @@ class EKF : public rclcpp::Node
       body_acc_sub_.subscribe(this, "dummy_body_acc");
       body_omega_sub_.subscribe(this, "dummy_body_omega");
 
-      sync_ = std::make_shared<message_filters::TimeSynchronizer<Position, Velocity, Pose, Acc, Omega>>
-                                                      (pos_sub_, vel_sub_, pose_sub_, body_acc_sub_, body_omega_sub_, 10);
-      
-      sync_->registerCallback(std::bind(&EKF::sync_callback, this, _1, _2, _3, _4, _5));
+      // for exact sync
+      // sync_ = std::make_shared<message_filters::TimeSynchronizer<Position, Velocity, Pose, Acc, Omega>>
+      //                                                 (pos_sub_, vel_sub_, pose_sub_, body_acc_sub_, body_omega_sub_, 10);
+      // for approximate sync
+      sync_.registerCallback(std::bind(&EKF::sync_callback, this, _1, _2, _3, _4, _5));
     }
 
   private:
@@ -55,6 +60,8 @@ class EKF : public rclcpp::Node
       // implement Extended Kalman Filter here
 
       RCLCPP_INFO(this->get_logger(), "I am at: (%.2f, %.2f, %.2f)", msg_pos->x, msg_pos->y, msg_pos->z);
+      
+      // Extended Kalman Filter
     }
 
     message_filters::Subscriber<Position> pos_sub_;
@@ -62,8 +69,12 @@ class EKF : public rclcpp::Node
     message_filters::Subscriber<Pose> pose_sub_;
     message_filters::Subscriber<Acc> body_acc_sub_;
     message_filters::Subscriber<Omega> body_omega_sub_;
-      
-    std::shared_ptr<message_filters::TimeSynchronizer<Position, Velocity, Pose, Acc, Omega>> sync_;
+
+    // for exact sync
+    // std::shared_ptr<message_filters::TimeSynchronizer<Position, Velocity, Pose, Acc, Omega>> sync_;
+    
+    // for approximate sync
+    Sync sync_;
 };
 
 int main(int argc, char * argv[])
@@ -78,3 +89,4 @@ int main(int argc, char * argv[])
 // https://answers.ros.org/question/361637/using-c-message-filters-in-ros2/
 // https://github.com/alsora/ros2-code-examples/blob/master/simple_time_sync/main.cpp
 // https://answers.ros.org/question/291876/how-can-i-subscribe-to-2-different-topics-in-a-synchronised-manner-in-ros2/
+// https://answers.ros.org/question/280856/synchronizer-with-approximate-time-policy-in-a-class-c/
