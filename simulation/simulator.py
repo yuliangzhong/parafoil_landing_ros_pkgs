61f996155fa41cd10100d73c75361a0593967c48
yuliangzhong <yubaby2003@163.com>
2022-08-05 00:52:18 +0200

    first version

--- simulation/simulator.py
+++ simulation/simulator.py
@@ -69,6 +69,9 @@ gravity_acc = + 9.81 # z-down
 # system constant
 system_mass = 2.2
 system_inertia = np.array([[1.68, 0, 0.09], [0, 0.80, 0], [0.09, 0, 0.32]]) # in body frame
+# system_inertia = np.eye(3) # in body frame
+# system_inertia_inv = np.eye(3)
+system_inertia_inv = np.array([[640./1059., 0, -60./353.], [0, 5./4., 0], [-60./353., 0, 1120./353.]]) # in body frame
 
 parafoil_ref_area = 1.5 # m2
 parafoil_ref_span = 1.88 # m
@@ -121,9 +124,6 @@ class Simulator(object):
         self.quat = matrix2quat(rpy2matrix3(np.array([0, 0.006, -10/180*pi], dtype=float64).reshape(-1,1)))
         self.vel = dot(quat2matrix(self.quat), np.array([3.819, -0.673, 1.62], dtype=float64).reshape(-1,1))
         self.ang_vel = np.zeros((3,1), dtype=float64)
-        # self.uvw = np.array([3.819, -0.673, 1.62], dtype=float64).reshape(-1,1)
-        # self.pqr = np.zeros((3,1), dtype=float64)
-        # self.rpy = np.array([0, 0.006, -10/180*pi], dtype=float64).reshape(-1,1)
 
         self.delta_l = 0.5 # [0,1]
         self.delta_r = 0.5 # [0,1]
@@ -134,14 +134,11 @@ class Simulator(object):
         print("vel: ", self.vel.reshape(-1))
         print("quat: ", self.quat.reshape(-1))
         print("omega: ", self.ang_vel.reshape(-1))
-        # print("rpy: ", self.rpy.reshape(-1))
-        # print("uvw: ", self.uvw.reshape(-1))
-        # print("pqr: ", self.pqr.reshape(-1))
         print("count: ", self.cnt)
         
-    # def action_callback(self, msg):
-    #     self.delta_r = max(0, min(1, msg.data.x))
-    #     self.delta_l = max(0, min(1, msg.data.y))
+    def action_callback(self, msg):
+        self.delta_r = max(0, min(1, msg.data.x))
+        self.delta_l = max(0, min(1, msg.data.y))
     
     # def wind_generator(self):
     #     if self.cnt % self.rand_int > self.rand_int - 5:
@@ -150,77 +147,82 @@ class Simulator(object):
     #     if self.cnt > 1e9:
     #         self.cnt = 1
 
-    def sim_forward(self):
+    def sim_step_forward(self):
         self.cnt += 1
 
-        ##### calculate total force and torque (without apparent mass) #####
-
-        # B_V_w = dot(rpy2matrix3(self.rpy).T, self.current_wind)
-        # B_v_IB_tilde = self.uvw - B_V_w
-        # P_v_IP_tilde = dot(C_BP.T,B_v_IB_tilde + cross_product(self.pqr, B_r_BP))
-        # delta_s = (self.delta_l + self.delta_r) / 2
-        # delta_a = self.delta_r - self.delta_l
-
-        # u, v, w = P_v_IP_tilde[0], P_v_IP_tilde[1], P_v_IP_tilde[2]
-        # alpha = atan(w/u) # angle of attack
-        # V_P = sqrt(u**2+v**2+w**2)
-        # beta = asin(v/V_P) # angle of sideslip
-        # C_PF = np.array([[cos(alpha)*cos(beta), -cos(alpha)*sin(beta), -sin(alpha)], \
-        #                  [sin(beta),            cos(beta),             0          ], \
-        #                  [sin(alpha)*cos(beta), -sin(alpha)*sin(beta), cos(alpha)]])
-
-        # P_w_IP_skew = dot(C_BP.T, dot(skew(self.pqr), C_BP))
-        # p, q, r = P_w_IP_skew[2,1], P_w_IP_skew[0,2], P_w_IP_skew[1,0]
-
-        # tmp = -np.array([cD[0] + alpha**2*cD[1] + delta_s*cD[2], beta * c_Yb, cL[0] + alpha*cL[1] + delta_s*cL[2]]).reshape(-1,1)
-        # P_F_A = 0.5 * rho * parafoil_ref_area * V_P**2 * dot(C_PF, tmp)
-
-        # c_l = dot(np.array([(parafoil_ref_span/2/V_P)*p, delta_a]), np.array([cM[0], cM[1]]).reshape(-1,1))
-        # c_m = dot(np.array([1, alpha, (parafoil_ref_length/2/V_P)*q]), np.array([cM[2], cM[3], cM[4]]).reshape(-1,1))
-        # c_n = dot(np.array([(parafoil_ref_span/2/V_P)*r, delta_a]), np.array([cM[5], cM[6]]).reshape(-1,1))
-        # P_M_A = 0.5 * rho * parafoil_ref_area * V_P**2 * np.array([parafoil_ref_span*c_l, parafoil_ref_length*c_m, parafoil_ref_span*c_n]).reshape(-1,1)
-    
-        # W_v_IW_tilde = dot(C_BW.T, (B_v_IB_tilde + cross_product(self.pqr, B_r_BW)))
-        # W_F_pd = -0.5*rho*S_pd*c_Dpd*np.linalg.norm(W_v_IW_tilde)*W_v_IW_tilde
-
-        # B_F_a = dot(C_BP, P_F_A) + dot(C_BW, W_F_pd)
-        # B_M_a = dot(C_BP, P_M_A) + cross_product(B_r_BP, P_F_A) + cross_product(B_r_BW, W_F_pd)
-
-        # B_G = system_mass*gravity_acc*np.array([-sin(self.rpy[1]), sin(self.rpy[0])*cos(self.rpy[1]), cos(self.rpy[0])*cos(self.rpy[1])]).reshape(-1,1)
-
-        # B_F = B_F_a + B_G
-        # B_M = B_M_a
-
-        # for debugging
-        B_F = np.array([0.2086225,   2.03626621, -1.24894524]).reshape(-1,1)
-        B_M = np.array([1.98817463, -5.57137594,  0.        ]).reshape(-1,1)
-        
         C_IB = quat2matrix(self.quat)
         I_ground = dot(dot(C_IB, system_inertia), C_IB.T)
-        ##### calculate system states by symplectic Euler method #####
-        vel_new = self.vel + dT/system_mass*dot(C_IB, B_F)
+        I_ground_inv = dot(dot(C_IB, system_inertia_inv), C_IB.T)
 
-        #     rb->state.orientation = updateRotationGivenAngularVelocity(rb->state.orientation,w_new,dt);
-        #     rb->state.velocity = v_new;
-        #     rb->state.angularVelocity = w_new;
-        self.pos += dT*vel_new
+        ##### calculate system states by symplectic Euler method #####
+        ##### x_n+1 = x_n + dT * v_n        ##### <----
+        ##### v_n+1 = v_n + dT * F(x_n+1)/M #####
+        self.pos += dT*self.vel
         nm = np.linalg.norm(self.ang_vel)
+
         if nm > 1e-10:
-            # print("in cos:",dT*nm/2)
+            # print("nm = ", nm)
             q_tmp0 = cos(dT*nm/2)
             q_tmph = sin(dT*nm/2)/nm*self.ang_vel
             q_tmp = np.vstack((np.array([[q_tmp0]]), q_tmph))
             self.quat = quat_product(q_tmp, self.quat)
-        else:
-            self.quat = self.quat
         
-        ang_vel_new = self.ang_vel + dT*dot(np.linalg.inv(I_ground),(dot(C_IB, B_M) - cross_product(self.ang_vel, dot(I_ground, self.ang_vel))))
+        ##### calculate total force and torque (without apparent mass) #####
+        rpy = matrix2rpy(quat2matrix(self.quat))
+        uvw = dot(C_IB.T, self.vel)
+        pqr = dot(C_IB.T, self.ang_vel)
 
-        self.vel = vel_new
-        self.ang_vel = ang_vel_new
-        # ground_vel_old = dot(rpy2matrix3(self.rpy), self.uvw)
-        # ground_vel_new = ground_vel_old + dT * dot(rpy2matrix3(self.rpy), B_F) / system_mass
-        # self.uvw = dot(rpy2matrix3(self.rpy).T, ground_vel_new)
+        B_V_w = dot(C_IB.T, self.current_wind)
+        B_v_IB_tilde = uvw - B_V_w
+        P_v_IP_tilde = dot(C_BP.T,B_v_IB_tilde + cross_product(pqr, B_r_BP))
+        delta_s = (self.delta_l + self.delta_r) / 2
+        delta_a = self.delta_r - self.delta_l
+
+        u, v, w = P_v_IP_tilde[0], P_v_IP_tilde[1], P_v_IP_tilde[2]
+        alpha = atan(w/u) # angle of attack
+        V_P = sqrt(u**2+v**2+w**2)
+        beta = asin(v/V_P) # angle of sideslip
+        C_PF = np.array([[cos(alpha)*cos(beta), -cos(alpha)*sin(beta), -sin(alpha)], \
+                         [sin(beta),            cos(beta),             0          ], \
+                         [sin(alpha)*cos(beta), -sin(alpha)*sin(beta), cos(alpha)]])
+
+        P_w_IP_skew = dot(C_BP.T, dot(skew(pqr), C_BP))
+        p, q, r = P_w_IP_skew[2,1], P_w_IP_skew[0,2], P_w_IP_skew[1,0]
+
+        tmp = -np.array([cD[0] + alpha**2*cD[1] + delta_s*cD[2], beta * c_Yb, cL[0] + alpha*cL[1] + delta_s*cL[2]]).reshape(-1,1)
+        P_F_A = 0.5 * rho * parafoil_ref_area * V_P**2 * dot(C_PF, tmp)
+
+        c_l = dot(np.array([(parafoil_ref_span/2/V_P)*p, delta_a]), np.array([cM[0], cM[1]]).reshape(-1,1))
+        c_m = dot(np.array([1, alpha, (parafoil_ref_length/2/V_P)*q]), np.array([cM[2], cM[3], cM[4]]).reshape(-1,1))
+        c_n = dot(np.array([(parafoil_ref_span/2/V_P)*r, delta_a]), np.array([cM[5], cM[6]]).reshape(-1,1))
+        P_M_A = 0.5 * rho * parafoil_ref_area * V_P**2 * np.array([parafoil_ref_span*c_l, parafoil_ref_length*c_m, parafoil_ref_span*c_n]).reshape(-1,1)
+    
+        W_v_IW_tilde = dot(C_BW.T, (B_v_IB_tilde + cross_product(pqr, B_r_BW)))
+        W_F_pd = -0.5*rho*S_pd*c_Dpd*np.linalg.norm(W_v_IW_tilde)*W_v_IW_tilde
+
+        B_F_a = dot(C_BP, P_F_A) + dot(C_BW, W_F_pd)
+        B_M_a = dot(C_BP, P_M_A) + cross_product(B_r_BP, P_F_A) + cross_product(B_r_BW, W_F_pd)
+
+        B_G = system_mass*gravity_acc*np.array([-sin(rpy[1]), sin(rpy[0])*cos(rpy[1]), cos(rpy[0])*cos(rpy[1])]).reshape(-1,1)
+
+        B_F = B_F_a + B_G
+        B_M = B_M_a
+
+        # # for debugging
+        # B_F = np.array([0.2086225,   2.03626621, -1.24894524]).reshape(-1,1)
+        # B_M = np.array([1.98817463, -5.57137594,  0.        ]).reshape(-1,1)
+        
+        ##### calculate system states by symplectic Euler method #####
+        ##### x_n+1 = x_n + dT * v_n        #####
+        ##### v_n+1 = v_n + dT * F(x_n+1)/M ##### <----
+        self.vel += dT/system_mass*dot(C_IB, B_F)
+
+        I_M = dot(C_IB, B_M)
+        cs = cross_product(self.ang_vel, dot(I_ground, self.ang_vel))
+        self.ang_vel += dT * (dot(I_ground_inv, I_M) - cs)
+        # self.ang_vel += dT * dot(I_ground_inv, I_M - cs)
+        # self.ang_vel += dT*(dot(np.linalg.inv(I_ground),dot(C_IB, B_M)) - cross_product(self.ang_vel, dot(I_ground, self.ang_vel)))
+        # self.ang_vel += dT*dot(np.linalg.inv(I_ground), dot(C_IB, B_M) - cross_product(self.ang_vel, dot(I_ground, self.ang_vel)))
 
 
     def sensor_pub(self):
@@ -230,9 +232,9 @@ class Simulator(object):
 
 if __name__ == '__main__':
     sim = Simulator()
-    for i in range(6000):
+    for i in range(2000):
         
-        sim.sim_forward()
+        sim.sim_step_forward()
         # plt.plot([count-1, count], [last_h, h], color='red')
         # plt.pause(0.001)
     sim.print_status()
