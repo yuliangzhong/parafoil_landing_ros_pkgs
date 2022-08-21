24bc330e5b40ca1704de91820d3441451c565d22
yuliangzhong <yubaby2003@163.com>
2022-08-21 21:09:14 +0200

    new launch file

--- project_launch/launch/all_sensors_launch.py
+++ project_launch/launch/all_sensors_launch.py
@@ -4,28 +4,53 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        # Node(
+        #     package='absolute_altimeter',
+        #     # namespace='turtlesim1',
+        #     executable='absolute_altimeter',
+        #     name='absolute_altimeter',
+        #     output='screen'
+        # ),
+        # Node(
+        #     package='system_monitor',
+        #     # namespace='turtlesim2',
+        #     executable='system_monitor',
+        #     name='system_monitor',
+        #     output='screen'
+        # ),
+        # Node(
+        #     package='magnetometer',
+        #     executable='magnetometer',
+        #     name='magnetometer',
+        #     output='screen'
+        # ),
         Node(
-            package='absolute_altimeter',
-            # namespace='turtlesim1',
-            executable='absolute_altimeter',
-            name='absolute_altimeter',
+            package='imu_gyro',
+            executable='imu_gyro',
+            name='imu_gyro',
             output='screen'
         ),
         Node(
-            package='system_monitor',
-            # namespace='turtlesim2',
-            executable='system_monitor',
-            name='system_monitor',
-            output='screen'
-        ),
-        Node(
-            package='magnetometer',
-            executable='magnetometer',
-            name='magnetometer',
+            package='state_estimator',
+            executable='state_estimator',
+            name='state_estimator',
             output='screen'
         ),
         ExecuteProcess(
-            cmd = ['ros2', 'bag', 'record', '/absolute_height', '/battery_voltage', '/magnetometer'],
+            cmd=[
+                'ros2', 'bag', 'record',
+                # all relevant topics
+                '-a',
+                '-x', '/rosout',  # exclude certain
+
+                # # all topics
+                # '-a',
+
+                # # list of topics
+                # '/absolute_height',
+                # '/battery_voltage',
+                # '/magnetometer',
+            ],
             output='screen'
         )
     ])
